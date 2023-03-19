4987169231f03de4877bbcfaa2ebfd7e64009728
arushi.pahuja@fortanix.com <arushi.pahuja@fortanix.com>
2023-03-19 20:04:18 +0530

    test

--- src/decode/avx2.rs
+++ src/decode/avx2.rs
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-use core::arch::x86_64::*;
+use core::arch::x86::*;
 
 use crate::avx2::*;
 

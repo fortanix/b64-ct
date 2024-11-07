1d21e6b1cbde3b360bd30bbfffda72fd17506e44
Thomas Van Strydonck <thomas.vanstrydonck@fortanix.com>
2024-11-07 17:44:35 +0100

    update wasi rust target to wasip1
    starting in rust 1.84, the wasi target is removed
    this broke the latest CI build on master

--- ci/install.sh
+++ ci/install.sh
@@ -8,7 +8,7 @@ if [ "$TRAVIS_OS_NAME" = "linux" ] && [ "$TRAVIS_CPU_ARCH" = "amd64" ]
 then
     rustup target add i686-unknown-linux-gnu
 
-    rustup target add wasm32-wasi
+    rustup target add wasm32-wasip1
     cargo install cargo-wasi
     curl -L https://github.com/CraneStation/wasmtime/releases/download/dev/wasmtime-dev-x86_64-linux.tar.xz \
         | sudo tar xJf - --strip-components=1 -C /usr/local/bin wasmtime-dev-x86_64-linux/wasmtime
@@ -18,5 +18,4 @@ then
 
     rustup target add x86_64-apple-darwin
     rustup target add aarch64-apple-darwin
-
 fi
